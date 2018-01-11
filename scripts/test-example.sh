04209d049c757645ac6bc17606b194719cffd82f
Oleksiy Golovko <alexei.golovko@gmail.com>
2018-01-11 13:17:02 +0100

    Fastpack: complete small integration refactoring

--- scripts/test-example.sh
+++ scripts/test-example.sh
@@ -9,41 +9,53 @@ C='\033[0;32m'
 NC='\033[0m'
 
 cd $EXAMPLE
+rm -rf fpack-test
 
-echo "${C}Production / Cache Purged${NC}"
+echo "${C}Regular / Production / Cache Purged${NC}"
 time $FPACK $ENTRY_POINT $FLAGS \
     --purge-cache \
-    -o fpack-test/regular.prod.js \
+    -o fpack-test/regular.prod.purge.js \
     --bundle regular \
     --mode production
-echo "${C}Development / Cache Purged${NC}"
+echo "${C}Regular / Development / Cache Purged${NC}"
 time $FPACK $ENTRY_POINT $FLAGS \
     --purge-cache \
-    -o fpack-test/regular.dev.js \
+    -o fpack-test/regular.dev.purge.js \
     --bundle regular \
     --mode development
-echo "${C}Production / Cache Used${NC}"
+echo "${C}Regular / Production / Cache Used${NC}"
 time $FPACK $ENTRY_POINT $FLAGS \
-    -o fpack-test/regular.prod.js \
+    -o fpack-test/regular.prod.cache.js \
     --bundle regular \
     --mode production
-echo "${C}Development / Cache Used${NC}"
+echo "${C}Regular / Development / Cache Used${NC}"
 time $FPACK $ENTRY_POINT $FLAGS \
-    -o fpack-test/regular.dev.js \
+    -o fpack-test/regular.dev.cache.js \
     --bundle regular \
     --mode development
-echo "${C}Production / Cache Ignored${NC}"
+echo "${C}Regular / Production / Cache Ignored${NC}"
 time $FPACK $ENTRY_POINT $FLAGS \
     --no-cache \
-    -o fpack-test/regular.prod.js \
+    -o fpack-test/regular.prod.no-cache.js \
     --bundle regular \
     --mode production
-echo "${C}Development / Cache Ignored${NC}"
+echo "${C}Regular / Development / Cache Ignored${NC}"
 time $FPACK $ENTRY_POINT $FLAGS \
     --no-cache \
-    -o fpack-test/regular.dev.js \
+    -o fpack-test/regular.dev.no-cache.js \
     --bundle regular \
     --mode development
+
+INDEX="fpack-test/index.html"
+cat <<HTML > $INDEX
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>${FILE}</title>
+  </head>
+  <body>
+    <ul>
+HTML
 
 for FILE in `cd fpack-test && ls *.js`; do
     cat  <<HTML > "fpack-test/${FILE%.*}.html"
@@ -58,4 +70,12 @@ for FILE in `cd fpack-test && ls *.js`; do
   </body>
 </html>
 HTML
+    echo "<li><a href='${FILE%.*}.html'>$FILE</a></li>" >> $INDEX
 done
+
+
+cat <<HTML >> $INDEX
+    </ul>
+  </body>
+</html>
+HTML
