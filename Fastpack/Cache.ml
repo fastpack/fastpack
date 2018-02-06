ab0cbcd1a12c792f0b0436015afa7cc72b8c00ae
Oleksiy Golovko <alexei.golovko@gmail.com>
2018-02-06 09:32:40 +0100

    Cache: rework interface

--- Fastpack/Cache.ml
+++ Fastpack/Cache.ml
@@ -1,25 +1,13 @@
+(* if no entry exists - read what is requested
+ * can be: existance, stats, file, package, module
+ * if entry exists and not trusted, validate all properties which are present
+ * if entry exists and trusted, but some properties are not present
+ * - read only those properties *)
+
+
 module StringSet = Set.Make(String)
 module M = Map.Make(String)
 module FS = FastpackUtil.FS
-
-module PathEntry = struct
-  type t = Unix.stats
-end
-
-module FileEntry = struct
-  type t = {
-    path : PathEntry.t;
-    digest : string;
-    content : string;
-  }
-end
-
-module PackageEntry = struct
-  type t = {
-    file : FileEntry.t;
-    package : Package.t;
-  }
-end
 
 module ModuleEntry = struct
   module Preprocessed = struct
@@ -39,132 +27,175 @@ module ModuleEntry = struct
   end
 
   type t = {
-    file: FileEntry.t;
     id : string;
-    package : Package.t option;
     preprocessed : Preprocessed.t option;
     analyzed : Analyzed.t option;
   }
 end
 
-type entry = Exists of bool
-           | Path of PathEntry.t
-           | File of FileEntry.t
-           | Package of PackageEntry.t
-           | Module of ModuleEntry.t
+type entry = {
+  exists : bool;
+  st_mtime : float;
+  st_kind : Unix.file_kind;
+  digest : string;
+  content : string;
+  package: Package.t option;
+  module_: ModuleEntry.t option;
+}
+
+type validate = Exists | Stats | File | Package | Module
 
 type t = {
   file_exists : string -> bool Lwt.t;
-  file_stat : string -> Unix.stats Lwt.t;
-  file_stat_opt : string -> Unix.stats option Lwt.t;
-  get_file : string -> (bool * FileEntry.t) Lwt.t;
-  get_package : string -> (bool * PackageEntry.t) Lwt.t;
+  file_stat : string -> (entry * bool) Lwt.t;
+  file_stat_opt : string -> (entry * bool) option Lwt.t;
+  get_file : string -> (entry * bool) Lwt.t;
+  get_package : string -> (Package.t * bool) Lwt.t;
 }
 
 exception FileDoesNotExist of string
 
 let create () =
+  let no_file = {
+    exists = false;
+    st_mtime = 0.0;
+    st_kind = Unix.S_REG;
+    digest = "";
+    content = "";
+    package = None;
+    module_ = None;
+  }
+  in
+
+  let trusted = ref StringSet.empty in
   let files = ref M.empty in
   (* TODO: try loading the cache *)
 
+  let update filename entry =
+    files := M.add filename entry !files;
+    trusted := StringSet.add filename !trusted
+  in
+
+  let validate _filename _entry =
+    failwith "Not imppl"
+  in
+
   let file_exists filename =
-    match M.get filename !files with
-    | Some (true, Exists exists) ->
+    match StringSet.mem filename !trusted, M.get filename !files with
+    | true, Some { exists; _} ->
       Lwt.return exists
-    | Some (true, _) ->
-      Lwt.return_true
-    | None | Some (false, _) ->
+    | _, None ->
       let%lwt exists = Lwt_unix.file_exists filename in
-      files := M.add filename (true, Exists exists) !files;
+      update filename { no_file with exists };
+      Lwt.return exists
+    | false, Some entry ->
+      let%lwt { exists; _ }, _ = validate filename entry in
       Lwt.return exists
   in
 
   let file_stat path =
-    match M.get path !files with
-    | None | Some (false, _) | Some (true, Exists true) ->
-      begin
-        match%lwt FS.stat_option path with
-        | Some stat ->
-          files := M.add path (true, Path stat) !files;
-          Lwt.return stat
-        | None ->
-          Lwt.fail (FileDoesNotExist path)
-      end
-    | Some (true, Exists false) ->
+    let read_stats () =
+      match%lwt FS.stat_option path with
+      | None ->
+        update path no_file;
+        Lwt.fail (FileDoesNotExist path)
+      | Some { st_mtime; st_kind; _} ->
+        let entry = { no_file with exists = true; st_mtime; st_kind } in
+        update path entry;
+        Lwt.return (entry, false)
+    in
+    match StringSet.mem path !trusted, M.get path !files with
+    | true, Some { exists = false; _ } ->
       Lwt.fail (FileDoesNotExist path)
-    | Some (true, Path path)
-    | Some (true, File { path; _ })
-    | Some (true, Package { file = { path; _ }; _})
-    | Some (true, Module { file = { path; _ }; _}) ->
-      Lwt.return path
+    | true, Some entry ->
+      if entry.st_mtime <> 0.0
+      then Lwt.return (entry, true)
+      else read_stats ()
+    | _, None ->
+      read_stats ()
+    | false, Some entry ->
+      let%lwt ({ exists; _ } as entry), cached = validate path entry in
+      if exists
+      then Lwt.return (entry, cached)
+      else Lwt.fail (FileDoesNotExist path)
   in
 
   let file_stat_opt path =
-    match M.get path !files with
-    | None | Some (false, _) | Some (true, Exists true) ->
-      begin
-        match%lwt FS.stat_option path with
-        | Some stat ->
-          files := M.add path (true, Path stat) !files;
-          Lwt.return_some stat
-        | None ->
-          files := M.add path (true, Exists false) !files;
-          Lwt.return_none
-      end
-    | Some (true, Exists false) ->
+    let read_stats () =
+      match%lwt FS.stat_option path with
+      | None ->
+        update path no_file;
+        Lwt.return_none
+      | Some { st_mtime; st_kind; _} ->
+        let entry = { no_file with exists = true; st_mtime; st_kind } in
+        update path entry;
+        Lwt.return_some (entry, false)
+    in
+    match StringSet.mem path !trusted, M.get path !files with
+    | true, Some { exists = false; _ } ->
       Lwt.return_none
-    | Some (true, Path path)
-    | Some (true, File { path; _ })
-    | Some (true, Package { file = { path; _ }; _ })
-    | Some (true, Module { file = { path; _ }; _}) ->
-      Lwt.return_some path
+    | true, Some entry ->
+      if entry.st_mtime <> 0.0
+      then Lwt.return_some (entry, true)
+      else read_stats ()
+    | _, None ->
+      read_stats ()
+    | false, Some entry ->
+      let%lwt ({ exists; _ } as entry), cached = validate path entry in
+      if exists
+      then Lwt.return_some (entry, cached)
+      else Lwt.return_none
   in
 
   let get_file filename =
-    match M.get filename !files with
-    | Some (true, File file)
-    | Some (true, Package { file; _ })
-    | Some (true, Module { file; _ }) ->
-      Lwt.return (true, file)
-    | _ ->
-      let%lwt path =
-        match%lwt file_stat_opt filename with
-        | None ->
-          Lwt.fail (FileDoesNotExist filename)
-        | Some ({ st_kind = Unix.S_REG; _} as path) ->
-          Lwt.return path
-        | _ ->
-          (* TODO: provide more meaningful error *)
-          Lwt.fail (FileDoesNotExist filename)
-      in
-      let%lwt content = Lwt_io.(with_file ~mode:Input filename Lwt_io.read) in
-      let digest = Digest.string content in
-      let file = {FileEntry. path; content; digest } in
-      files := M.add filename (true, File file) !files;
-      Lwt.return (false, file)
+    let read_file () =
+      let%lwt stats = file_stat_opt filename in
+      match stats with
+      | Some (entry, _) ->
+        let%lwt content = Lwt_io.(with_file ~mode:Input filename read) in
+        let digest = Digest.string content in
+        let entry = { entry with content; digest } in
+        update filename entry;
+        Lwt.return (entry, false)
+      | None ->
+        Lwt.fail (FileDoesNotExist filename)
+    in
+    match StringSet.mem filename !trusted, M.get filename !files with
+    | true, Some { exists = false; _ } ->
+      Lwt.fail (FileDoesNotExist filename)
+    | true, Some entry ->
+      if entry.digest <> ""
+      then Lwt.return (entry, true)
+      else read_file ()
+    | _, None ->
+      read_file ()
+    | false, Some entry ->
+      let%lwt ({ exists; _ } as entry), cached = validate filename entry in
+      if exists
+      then Lwt.return (entry, cached)
+      else Lwt.fail (FileDoesNotExist filename)
   in
 
   let get_package filename =
-    match M.get filename !files with
-    | Some (true, Package package) ->
-      Lwt.return (true, package)
-    | Some (true, Module { package = Some package; file; _ }) ->
-      Lwt.return (true, { PackageEntry. file; package })
-    | Some (true, Module { package = None; _ }) ->
-      (* TODO: check if filename ends with /package.json
-       * If so - construct package, update module entry
-       * otherwise - fail
-       * *)
-      failwith ("Module is not package.json: " ^ filename)
-    | _ ->
-      let%lwt (cached, file) = get_file filename in
-      let package = { PackageEntry.
-          file;
-          package = Package.of_json filename file.FileEntry.content
-        }
-      in
-      files := M.add filename (true, Package package) !files;
-      Lwt.return (cached, package)
+    match StringSet.mem filename !trusted, M.get filename !files with
+    | true, Some { package = Some package; _ } ->
+      Lwt.return (package, true)
+    | _, None
+    | true, Some _ ->
+      let%lwt entry, cached = get_file filename in
+      let package = Package.of_json filename entry.content in
+      update filename { entry with package = Some package };
+      Lwt.return (package, cached)
+    | false, Some entry ->
+      let%lwt { exists; package; _ }, cached = validate filename entry in
+      match exists, package with
+      | false, _ ->
+        Lwt.fail (FileDoesNotExist filename)
+      | true, Some package ->
+        Lwt.return (package, cached)
+      | true, None ->
+        (* TODO: not a package.json error *)
+        Lwt.fail (FileDoesNotExist filename)
   in
 
 
