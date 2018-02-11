75a94d111162dfcc53d91ca87c98d8b47ceb6866
Oleksiy Golovko <alexei.golovko@gmail.com>
2018-02-11 13:23:57 +0100

    Preprocessor: reimplement config

--- Fastpack/Preprocessor.ml
+++ Fastpack/Preprocessor.ml
@@ -11,8 +11,7 @@ type processor = Builtin | Node of string
 type config = {
   pattern_s : string;
   pattern : Re.re;
-  processor : processor;
-  options : option_value M.t;
+  processors : string list;
 }
 
 type process_f = string -> string * string list
@@ -27,15 +26,37 @@ type t = {
 let config_re = Re_posix.compile_pat "^([^:]+)(:([a-zA-Z_][^?]+)(\\?(.*))?)?$"
 
 let of_string s =
-  let pattern_s, processor, options =
-    match Re.exec_opt config_re @@ String.trim s with
-    | None ->
-      raise (Failure "Incorrect config")
-    | Some groups ->
-      let parts = Re.Group.all groups in
-      String.trim parts.(1), String.trim parts.(3), String.trim parts.(5)
+  let pattern_s, processors =
+    match String.(s |> trim |> split_on_char ':') with
+    | [] | [""] ->
+      raise (Failure "Empty config")
+    | pattern_s :: "" :: [] ->
+      pattern_s, ["builtin"]
+    | pattern_s :: rest ->
+      let processors =
+        String.(rest |> concat ":" |> split_on_char '!')
+        |> List.filter_map
+          (fun s ->
+             let s = String.trim s in
+             match s = "" with
+             | true -> None
+             | false ->
+               match String.split_on_char '?' s  with
+               | [] ->
+                 raise (Failure "Empty processor")
+               | processor :: [] ->
+                 Some processor
+               | "builtin" :: "" :: [] ->
+                 Some "builtin"
+               | processor :: opts :: [] when processor <> "builtin" ->
+                 Some (processor ^ "?" ^ opts)
+               | _ ->
+                 raise (Failure "Incorrect preprocessor config")
+
+          )
+      in
+      pattern_s, processors
   in
-  let processor = if processor = "" then "builtin" else processor in
   let pattern =
     try
       Re_posix.compile_pat pattern_s
@@ -43,62 +64,14 @@ let of_string s =
     | Re_posix.Parse_error ->
       raise (Failure "Pattern regexp parse error. Use POSIX syntax")
   in
-  if processor = "builtin" && options <> ""
-  then raise (Failure "'builtin' processor does not expect any options (yet!)");
-  let processor =
-    match processor with
-    | "builtin" -> Builtin
-    | _ -> Node processor
-  in
-  let option_value_of_string s =
-    match s with
-    | "" | "true" -> Boolean true
-    | "false" -> Boolean false
-    | _ ->
-      try
-        Number (float_of_string s)
-      with
-      | Failure _ ->
-        String s
-  in
-  let options =
-    options
-    |> String.split_on_char '&'
-    |> List.filter_map
-      (fun s -> if s = "" then None else Some (String.split_on_char '=' s))
-    |> List.map
-      (fun parts ->
-         List.hd parts,
-         List.tl parts |> String.concat "" |> option_value_of_string
-      )
-    |> List.fold_left (fun opts (k, v) -> M.add k v opts) M.empty
-  in
-  { pattern_s; pattern; processor; options }
+  { pattern_s; pattern; processors }
 
-let to_string { pattern_s; processor; options; _ } =
-  let option_value_to_string opt =
-    match opt with
-    | Boolean true -> "true"
-    | Boolean false -> "false"
-    | Number n -> string_of_float n
-    | String s -> s
-  in
-  let options =
-    options
-    |> M.bindings
-    |> List.map (fun (k, v) -> k ^ "=" ^ option_value_to_string v)
-    |> String.concat ""
-  in
-  let processor =
-    match processor with
-    | Builtin -> "builtin"
-    | Node name -> name
-  in
+
+let to_string { pattern_s; processors; _ } =
   Printf.sprintf
-    "%s:%s%s"
+    "%s:%s"
     pattern_s
-    processor
-    (if options <> "" then "?" ^ options else "")
+    (String.concat "!" processors)
 
 
 let all_transpilers = FastpackTranspiler.[
@@ -213,40 +186,38 @@ end
 
 let make configs =
 
-  let _processors = ref M.empty in
+  let processors = ref M.empty in
 
-  let get_processors _filename =
-    failwith "reimplement"
-    (* match M.get filename !processors with *)
-    (* | Some processors -> processors *)
-    (* | None -> *)
-    (*   let p = *)
-    (*     configs *)
-    (*     |> List.filter_map *)
-    (*       (fun { pattern; processor; options; _ } -> *)
-    (*         match Re.exec_opt pattern filename with *)
-    (*         | None -> None *)
-    (*         | Some _ -> *)
-    (*           match processor with *)
-    (*           | Builtin -> Some builtin *)
-    (*           | Node loader -> Some (NodeServer.process loader options) *)
-    (*       ) *)
-    (*   in *)
-    (*   processors := M.add filename p !processors; *)
-    (*   p *)
+  let get_processors filename =
+    match M.get filename !processors with
+    | Some processors -> processors
+    | None ->
+      let p =
+        configs
+        |> List.fold_left
+          (fun acc { pattern; processors; _ } ->
+            match Re.exec_opt pattern filename with
+            | None -> acc
+            | Some _ -> acc @ processors
+          )
+          []
+      in
+      processors := M.add filename p !processors;
+      p
   in
 
-  let process filename source =
-    let%lwt source, build_dependencies =
-      Lwt_list.fold_left_s
-        (fun (source, dependencies) processor ->
-           let%lwt (source, new_dependencies) = processor source in
-           Lwt.return (source, dependencies @ new_dependencies)
-        )
-        (source, [])
-        (get_processors filename)
-    in
-    Lwt.return (source, build_dependencies)
+  let process _filename _source =
+    failwith "not implemented"
+    (* let%lwt source, build_dependencies = *)
+    (*   Lwt_list.fold_left_s *)
+    (*     (fun (source, dependencies) processor -> *)
+    (*        let%lwt (source, new_dependencies) = processor source in *)
+    (*        Lwt.return (source, dependencies @ new_dependencies) *)
+    (*     ) *)
+    (*     (source, []) *)
+    (*     (get_processors filename) *)
+    (* in *)
+    (* Lwt.return (source, build_dependencies) *)
   in
 
   Lwt.return { get_processors; process; configs; finalize = NodeServer.finalize }
