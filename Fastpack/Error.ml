af8b9eb8b7c3d61e74c3d4172704f1b301c3bb69
Jake F - @samouri <samouri@users.noreply.github.com>
2018-07-24 14:04:39 -0400

    standardize headers

--- Fastpack/Error.ml
+++ Fastpack/Error.ml
@@ -1,7 +1,7 @@
 module Loc = FlowParser.Loc
 module Scope = FastpackUtil.Scope
 
-(* 
+(*
   assoc list of nodejs libs and their mock implementations
   as listed on: https://github.com/webpack/node-libs-browser
 *)
@@ -39,52 +39,62 @@ let nodelibs = [
   "util", Some "util";
   "vm", Some "vm-browserify";
   "zlib", Some "browserify-zlib";
-] 
+]
 
 type color = Cyan | Red | Black | White;;
 type font = Regular | Bold;;
 
-let print_with_color ?font:(font=Regular) str col = 
-  let col = match col with 
+let print_with_color ?(font=Regular) ?(isTTY=true) str col =
+  let col = match col with
     | Cyan -> "36"
     | Red -> "31"
     | Black -> "30"
     | White -> "37"
   in
-  let f = match font with 
+  let f = match font with
     | Regular -> "0"
     | Bold -> "1"
   in
-  "\027[" ^ f ^ ";" ^ col ^ "m" ^ str ^ "\027[0m"
+  if isTTY then
+    "\027[" ^ f ^ ";" ^ col ^ "m" ^ str ^ "\027[0m"
+  else
+    str
 
-let get_codeframe ?(isTTY=false) (loc: Loc.t) lines = 
+let format_error_header ?(isTTY=true) ?(subtitle="") (title, path) =
+  if isTTY then
+    (print_with_color title Red) ^ " " ^ subtitle,
+    print_with_color ~font: Bold path Cyan
+  else
+    title ^ " " ^ subtitle, path
+
+let get_codeframe ?(isTTY=false) (loc: Loc.t) lines =
   let startLine = max 0 (loc.start.line - 2) in
   let endLine = min (List.length lines) (loc._end.line + 1) in
-  let codeframe = List.filter_map (fun (i, line) -> 
-      if startLine <= i  && i <= endLine then 
-        Some (i, line) 
+  let codeframe = List.filter_map (fun (i, line) ->
+      if startLine <= i  && i <= endLine then
+        Some (i, line)
       else None
     ) lines
   in
   let maxLineNo = List.fold_left (fun n (i, _) -> max n i) 0 lines in
   let maxDigits = String.length (string_of_int maxLineNo) in
-  let formatted = List.map (fun (i, line) -> 
+  let formatted = List.map (fun (i, line) ->
       let isErrorLine = loc.start.line <= i && i <= loc._end.line in
       let lineNo = String.pad maxDigits (string_of_int i) in
       match isErrorLine, isTTY with
-      | false, _ -> lineNo ^ " │ " ^ line 
-      | true, false -> 
+      | false, _ -> lineNo ^ " │ " ^ line
+      | true, false ->
         let offset, length = (loc.start.column + 1), (loc._end.column - loc.start.column) in
         let whitespaceBeforeBar = String.repeat " " (maxDigits + 1) in
         let whitespaceAfterBar = String.repeat " " offset in
         let carets = String.repeat "^" length in
         lineNo ^ " │ " ^ line ^ "\n" ^ whitespaceBeforeBar ^ "│" ^ whitespaceAfterBar ^ carets
-      | true, true -> 
+      | true, true ->
         let error_substring = FastpackUtil.UTF8.sub line (loc.start.column) (loc._end.column - loc.start.column) in
         let colored_error = print_with_color error_substring Red in
         let colored_line = String.replace ~sub:error_substring ~by: colored_error line in
-        (print_with_color lineNo Red) ^ " │ " ^ colored_line  
-    ) codeframe in 
+        (print_with_color lineNo Red) ^ " │ " ^ colored_line
+    ) codeframe in
   String.concat "\n" formatted
 type reason =
   | CannotReadModule of string
@@ -118,24 +128,36 @@ let to_string package_dir error =
   | CannotLeavePackageDir filename ->
     Printf.sprintf
       "%s is out of the working directory\n"
-      filename 
+      filename
 
   | CannotResolveModule (_, dep) ->
-    let dep_str = Module.Dependency.to_string ~dir:(Some package_dir) dep in
-    let error_msg = "Cannot resolve request for " ^ dep_str in
+    let isTTY = FastpackUtil.FS.isatty Unix.stderr in
+    let (error_title, location) = format_error_header ~isTTY ~subtitle: ("cannot resolve '" ^ dep.request ^ "'") (
+        "Module resolution error:",
+        Module.location_to_string dep.requested_from
+      )
+    in
     (match List.assoc_opt dep.request nodelibs with
-     | None -> error_msg ^ "\n"
+     | None -> String.concat "\n" [
+         error_title;
+         location;
+       ]
      | Some None -> String.concat "\n" [
-         error_msg;
+         error_title;
+         location;
          "This looks like base node.js library which does not have any browser implementation we are aware of";
        ]
      | Some Some mock -> String.concat "\n" [
-         error_msg;
+         error_title;
+         location;
          "This looks like base node.js library and unlikely is required in the browser environment.";
          "If you still want to use it, first install the browser implementation with:";
-         Printf.sprintf "npm install --save %s" mock;
+         "";
+         Printf.sprintf "\t\tnpm install --save %s" mock;
+         "";
          "And then add this command line option when running fpack:";
-         Printf.sprintf "--mock %s:%s" dep.request mock;
+         "";
+         Printf.sprintf "\t\t--mock %s:%s" dep.request mock;
        ]
     )
 
@@ -144,28 +166,23 @@ let to_string package_dir error =
     let lines = String.split_on_char '\n' source
                 |> List.mapi (fun i line -> (i + 1, line)) in
 
-    let format_error isTTY (loc, error) = 
-      let error_desc = FlowParser.Parse_error.PP.error error in 
+    let format_error isTTY (loc, error) =
+      let error_desc = FlowParser.Parse_error.PP.error error in
       String.concat "\n" [
         "--------------------";
         error_desc ^ " at " ^ (loc_to_string loc);
         "";
         (get_codeframe ~isTTY loc lines);
-        ""; 
+        "";
       ]
     in
-    let (error_title, location) = 
-      if isTTY then
-        (print_with_color "Parse Error\n" Red, 
-         print_with_color ~font:Bold location_str Cyan)
-      else 
-        ("Parse Error\n", location_str) 
-    in
+    let (error_title, location) = format_error_header ~isTTY ("Parse error", location_str) in
     String.concat "\n" [
-      error_title ^ location;
+      error_title;
+      location;
       "";
       String.concat "\n" (List.map (format_error isTTY) errors);
-      ""; 
+      "";
     ]
 
   | NotImplemented (some_loc, message) ->
