326e9737d8a9e961a575a535c5eba22e2dafd131
Oleksiy Golovko <alexei.golovko@gmail.com>
2018-10-31 15:34:54 +0100

    add some integration tests to the CI for Windows

--- test/pack-cra/dev.test.js
+++ test/pack-cra/dev.test.js
@@ -2,7 +2,7 @@
 module.exports = ({ bundle }) => bundle(`
 fpack src/index.js \
     --dev \
-    --preprocess='^src.+\\.js$' \
-    --preprocess='\\.svg$:url-loader' \
-    --preprocess='\\.css$:style-loader!css-loader?importLoaders=1!postcss-loader?path=postcss.config.js'
+    --preprocess=^src.+\\.js$ \
+    --preprocess=\\.svg$:url-loader \
+    --preprocess=\\.css$:style-loader!css-loader?importLoaders=1!postcss-loader?path=postcss.config.js
 `);
