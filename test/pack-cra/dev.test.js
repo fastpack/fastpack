16b5e1a48b9ebcadae5c03eab0ce1738b8421dd9
Oleksiy Golovko <alexei.golovko@gmail.com>
2019-05-07 08:39:07 +0200

    fix test/pack-cra

--- test/pack-cra/dev.test.js
+++ test/pack-cra/dev.test.js
@@ -1,5 +1,12 @@
 
-module.exports = ({ bundle }) => bundle(`
+/*
+ * We are using bundleNoDiff here since babel-loader adds absolute path
+ * when transpiling JSX, hence we can't have the idempotent bundle
+ * The best we can check here is the return code and also manual inspection
+ * of the changes
+ */
+
+module.exports = ({ bundleNoDiff }) => bundleNoDiff(`
 fpack build src/index.js \
     --dev \
     --preprocess=^src.+\\.js$:babel-loader \
